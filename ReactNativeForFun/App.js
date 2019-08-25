0e54591a3d6745d5c5b248a7fe8a8848ea5e1c92
Hao Ruan <haoru@cisco.com>
2019-08-25 22:30:39 +0800

    Add some examples for Transformation

--- ReactNativeForFun/App.js
+++ ReactNativeForFun/App.js
@@ -3,7 +3,14 @@
 // export default from './Demo/MarginProperties/App.js';
 // export default from './Demo/PaddingProperties/App.js';
 // export default from './Demo/TextProperties/App.js';
+// export default from './Demo/ShadowProperties/App.js';
+// export default from './Demo/ElevationProperties/App.js';
 
+
+// export default from './Demo/TransformationTranslate/App.js';
+// export default from './Demo/TransformationRotateX/App.js';
+// export default from './Demo/TransformationRotateY/App.js';
+// export default from './Demo/TransformationRotateZ/App.js';
 // export default from './Demo/TransformationScale/App.js';
 // export default from './Demo/TransformationSkew/App.js';
 
@@ -12,10 +19,11 @@
 // export default from './Demo/FlexJustify/App.js';
 // export default from './Demo/FlexAlignItems/App.js';
 // export default from './Demo/FlexAlignSelf/App.js';
-export default from './Demo/FlexWrap/App.js';
+// export default from './Demo/FlexWrap/App.js';
 
 
 
 // export default from './Demo/ToggleBackgroundColor/App.js';
 // export default from './Demo/ProfileCard/App.js';
 // export default from './Demo/ProfileCardThumbnail/App.js';
+export default from './Demo/BackfaceVisibility/App.js';
