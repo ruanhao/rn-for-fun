40b73d7f4b8c2be95be988a5977662e93e3ae768
Hao Ruan <haoru@cisco.com>
2019-08-25 21:38:57 +0800

    Add some examples for Flex

--- ReactNativeForFun/App.js
+++ ReactNativeForFun/App.js
@@ -3,8 +3,18 @@
 // export default from './Demo/MarginProperties/App.js';
 // export default from './Demo/PaddingProperties/App.js';
 // export default from './Demo/TextProperties/App.js';
+
 // export default from './Demo/TransformationScale/App.js';
-export default from './Demo/TransformationSkew/App.js';
+// export default from './Demo/TransformationSkew/App.js';
+
+// export default from './Demo/Flexbox/App.js';
+// export default from './Demo/FlexDirection/App.js';
+// export default from './Demo/FlexJustify/App.js';
+// export default from './Demo/FlexAlignItems/App.js';
+// export default from './Demo/FlexAlignSelf/App.js';
+export default from './Demo/FlexWrap/App.js';
+
+
 
 // export default from './Demo/ToggleBackgroundColor/App.js';
 // export default from './Demo/ProfileCard/App.js';
