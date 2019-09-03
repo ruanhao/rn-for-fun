c51f1018e2cab218a916ddef5b10740a7be0fad8
Hao Ruan <haoru@cisco.com>
2019-09-03 11:23:17 +0800

    [Hello Navigation] - Passing params to routes

--- ReactNativeForFun/Demo/HelloNavigation/App.js
+++ ReactNativeForFun/Demo/HelloNavigation/App.js
@@ -13,7 +13,10 @@ class HomeScreen extends React.Component {
                     title='Go to Details'
                     /* the `navigation` prop is passed in to every screen component in stack navigator */
                     /*If we call this.props.navigation.navigate with a route name that we haven't defined on a stack navigator, nothing will happen.*/
-                    onPress={() => this.props.navigation.navigate('Details')}
+                    onPress={() => this.props.navigation.navigate('Details', { /* 1. Navigate to the Details route with params */
+                        itemId: 86,
+                        otherParam: 'Coca Cola',
+                    })}
                 />
             </View>
         );
@@ -22,9 +25,25 @@ class HomeScreen extends React.Component {
 
 class DetailsScreen extends React.Component {
     render() {
+
+        /* 2. Get the param, provide a fallback value if not available */
+
+        /* You can also directly access the params object with `this.props.navigation.state.params`.
+           This may be `null` if no params were supplied, and so it's usually easier to just use getParam so you don't have to deal with that case.
+         */
+
+        /* If you want to access the params directly through props (eg. `this.props.itemId`) rather than `this.props.navigation.getParam`,
+           you may use a community-developed react-navigation-props-mapper package (https://github.com/vonovak/react-navigation-props-mapper).
+         */
+        const { navigation } = this.props;
+        const itemId = navigation.getParam('itemId', 'NO-ID');
+        const otherParam = navigation.getParam('otherParam', 'Sprite');
+
         return (
             <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
                 <Text>Details Screen</Text>
+                <Text>itemId: {JSON.stringify(itemId)}</Text>
+                <Text>otherParam: {JSON.stringify(otherParam)}</Text>
                 <Button
                     title="Go to Details... (nothing will happen)"
                     /*If you are already on that screen then it makes sense that it would do nothing*/
@@ -33,7 +52,9 @@ class DetailsScreen extends React.Component {
                 <Button
                     title="Go to Details... (push)"
                     /*This allows us to express the intent to add another route REGARDLESS OF THE EXISTING NAVIGATION HISTORY.*/
-                    onPress={() => this.props.navigation.push('Details')}
+                    onPress={() => this.props.navigation.push('Details', {
+                        itemId: Math.floor(Math.random() * 100),
+                    })}
                 />
                 <Button
                     title="Go to Home"
